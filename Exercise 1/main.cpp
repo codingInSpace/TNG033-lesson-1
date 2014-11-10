634328bd1d69642b6cd3a9debbc49cb6dc4010d4
Jonathan <jonathan.grangien@gmail.com>
2014-11-10 11:01:55 +0100

    Create basics file

---
+++ Exercise 1/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+struct Vector{
+
+
+};
+
+int main(){
+
+
+	return 0;
+}
