b2b015dfc76c7b39d489dc3fb2f930b54bf7dd49
Jonathan <jonathan.grangien@gmail.com>
2014-11-10 11:21:06 +0100

    add function declarations

--- Exercise 1/main.cpp
+++ Exercise 1/main.cpp
@@ -11,7 +11,26 @@ struct Vector{
 	int *p;		//pointer to vector
 };
 
+//Create a vector with n columns
+//Allocate an array
+void create_vector(Vector &V, int n = 0);
 
+//Deallocate space occupied by vector
+//V = empty vector
+void empty_vector(Vector &V);
+
+//Assuming V not empty, return number of columns
+int get(const Vector &V, int i);
+
+//Set number of columns
+void set(Vector &V, int i);
+
+//Display Vector V to stream out
+ostream& operator<<(ostream& out, const Vector &V);
+
+
+//Read a Vector V from stream in
+istream& operator>>(istream& in, Vector &V);
 
 int main(){
 
